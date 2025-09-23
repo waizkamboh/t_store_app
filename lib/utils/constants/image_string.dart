d3a7ffdbee0f4798eb8d2d4337a5ddd5c684ec32
waizkamboh <waizkamboh6@gmail.com>
2025-09-23 16:27:55 +0500

    Add Enums, text_string, images, sizes classes

---
+++ lib/utils/constants/image_string.dart
@@ -0,0 +1,110 @@
+/* -- App Image Strings -- */
+
+/// This class contains all the App Images in String formats.
+class TImages {
+  // -- Splash Screen Images
+  static const String tSplashTopIcon = "assets/images/splash_images/splash-top-icon.png";
+  static const String tSplashImage = "assets/images/splash_images/splash-screen-image.png";
+
+  // -- OnBoarding Screen Images
+  static const String tOnBoardingImage1 = "assets/images/on_boarding_images/on-boarding-image-1.png";
+  static const String tOnBoardingImage2 = "assets/images/on_boarding_images/on-boarding-image-2.png";
+  static const String tOnBoardingImage3 = "assets/images/on_boarding_images/on-boarding-image-3.png";
+
+  // -- Welcome Screen Image [Used in Login & SignUp]
+  static const String tWelcomeScreenImage = "assets/images/welcome_images/welcome-screen-image.png";
+  static const String tLogoImage = "assets/images/profile/logo.png";
+
+  // -- SOCIAL Images
+  static const String tFacebookLogo = "assets/logo/facebook-logo.png";
+  static const String tGoogleLogo = "assets/logo/google-logo.png";
+
+  // -- Forget Password Images
+  static const String tForgetPasswordImage = "assets/images/forget_password/forget-password.png";
+
+  // -- Dashboard Images
+  static const String tUserProfileImage = "assets/images/dashboard/dash-person.png";
+  static const String tBookmarkIcon = "assets/images/dashboard/bookmark-icon.png";
+  static const String tBannerImage1 = "assets/images/dashboard/dash-01.png";
+  static const String tBannerImage2 = "assets/images/dashboard/dash-02.png";
+  static const String tTopCourseImage1 = "assets/images/dashboard/dash-03.png";
+  static const String tTopCourseImage2 = "assets/images/dashboard/dash-04.png";
+
+  // -- Profile Images
+  static const String tProfileImage = "assets/images/profile/profile-pic.png";
+
+  // -- Phone Authentication Images
+  static String signInAnimation = "assets/images/animations/Animation - 1734020174401.json";
+  static String docerAnimation = "assets/images/animations/141594-animation-of-docer.json";
+
+  // -- Email Verification Images
+  static const String deliveredEmailIllustration = "assets/images/animations/sammy-line-man-receives-a-mail.png";
+  static String successfullyRegisterAnimation = "assets/images/animations/72462-check-register.json";
+
+  // -- Category Icons
+  static const String sportIcon = "assets/icons/categories/icons8-bowling-64.png";
+  static const String clothIcon = "assets/icons/categories/icons8-tailors-dummy-64.png";
+  static const String shoeIcon = "assets/icons/categories/icons8-shoes-64.png";
+  static const String cosmeticsIcon = "assets/icons/categories/icons8-cosmetics-64.png";
+  static const String animalIcon = "assets/icons/categories/icons8-dog-heart-64.png";
+  static const String toyIcon = "assets/icons/categories/icons8-wooden-toy-car-50.png";
+  static const String furnitureIcon = "assets/icons/categories/icons8-dining-chair-64.png";
+  static const String jeweleryIcon = "assets/icons/categories/icons8-sparkling-diamond-64.png";
+  static const String electronicsIcon = "assets/icons/categories/icons8-smartphone-64.png";
+
+  //Banners
+  static const String promoBanner1 = "assets/images/banners/promo-banner-1.png";
+  static const String promoBanner2 = "assets/images/banners/promo-banner-2.png";
+  static const String promoBanner3 = "assets/images/banners/promo-banner-3.png";
+  static const String banner2 = "assets/images/banners/banner_2.jpg";
+  static const String banner3 = "assets/images/banners/banner_3.jpg";
+  static const String banner4 = "assets/images/banners/banner_4.jpg";
+
+  // -- Payment Methods
+  static const String applePay = "assets/icons/payment_methods/apple-pay.png";
+  static const String googlePay = "assets/icons/payment_methods/google-pay.png";
+  static const String creditCard = "assets/icons/payment_methods/credit-card.png";
+  static const String masterCard = "assets/icons/payment_methods/master-card.png";
+  static const String paypal = "assets/icons/payment_methods/paypal.png";
+  static const String visa = "assets/icons/payment_methods/visa.png";
+  static const String paystack = "assets/icons/payment_methods/paystack.png";
+  static const String paytm = "assets/icons/payment_methods/paytm.png";
+  static const String successfulPaymentIcon = "assets/icons/payment_methods/successful_payment_icon.png";
+
+  // -- Brand Icons
+  static const String nikeLogo = "assets/icons/brands/nike.png";
+  static const String zaraLogo = "assets/icons/brands/zara-logo.png";
+  static const String appleLogo = "assets/icons/brands/apple-logo.png";
+
+  // -- Products
+  static const String productImage1 = "assets/images/products/nike-shoes.png";
+  static const String productImage5 = "assets/images/products/product-shirt.png";
+  static const String productImage7 = "assets/images/products/NikeAirJOrdonBlackRed.png";
+  static const String productImage8 = "assets/images/products/NikeAirJOrdonOrange.png";
+  static const String productImage9 = "assets/images/products/NikeAirJordonwhiteMagenta.png";
+  static const String productImage10 = "assets/images/products/NikeAirJOrdonWhiteRed.png";
+  static const String productImage11 = "assets/images/products/samsung_s9_mobile.png";
+  static const String productImage12 = "assets/images/products/samsung_s9_mobile_withback.png";
+  static const String productImage13 = "assets/images/products/samsung_s9_mobile_back.png";
+  static const String productImage18 = "assets/images/products/tomi_dogfood.png";
+  static const String productImage19 = "assets/images/products/NikeAirJordonSingleBlue.png";
+  static const String productImage20 = "assets/images/products/NikeAirJordonSingleOrange.png";
+  static const String productImage21 = "assets/images/products/NikeAirMax.png";
+  static const String productImage22 = "assets/images/products/NikeBasketballShoeGreenBlack.png";
+  static const String productImage23 = "assets/images/products/NikeWildhorse.png";
+  static const String productImage60 = "assets/images/products/tshirt_red_collar.png";
+  static const String productImage61 = "assets/images/products/tshirt_yellow_collar.png";
+  static const String productImage62 = "assets/images/products/tshirt_green_collar.png";
+  static const String productImage63 = "assets/images/products/tshirt_blue_collar.png";
+  static const String productImage64 = "assets/images/products/leather_jacket_1.png";
+  static const String productImage65 = "assets/images/products/leather_jacket_2.png";
+  static const String productImage66 = "assets/images/products/leather_jacket_3.png";
+  static const String productImage67 = "assets/images/products/leather_jacket_4.png";
+  static const String productImage68 = "assets/images/products/tshirt_blue_without_collar_back.png";
+  static const String productImage69 = "assets/images/products/tshirt_blue_without_collar_front.png";
+
+
+  //Profile
+  static const String user = "assets/images/content/user.png";
+
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,171 @@
+/* -- App Text Strings -- */
+
+/// This class contains all the App Text in String formats.
+class TTexts {
+  // -- GLOBAL Texts
+  static const String tNo = "No";
+  static const String and = "and";
+  static const String tYes = "Yes";
+  static const String skip = "Skip";
+  static const String done = "Done";
+  static const String tNext = "Next";
+  static const String tLogin = "Login";
+  static const String email = "E-Mail";
+  static const String submit = "Submit";
+  static const String tEmail = "E-Mail";
+  static const String tSignup = "Signup";
+  static const String tLogout = "Logout";
+  static const String appName = "T-Store";
+  static const String ohSnap = "Oh Snap!";
+  static const String tSuccess = "Success";
+  static const String tPhoneNo = "Phone No";
+  static const String password = "Password";
+  static const String tContinue = "Continue";
+  static const String tPassword = "Password";
+  static const String tFullName = "Full Name";
+  static const String tGetStarted = "Get Started";
+  static const String tForgetPassword = "Forget Password?";
+  static const String homeAppbarSubTitle = "Coding With T";
+  static const String tSignInWithGoogle = "Sign-In with Google";
+
+  // -- Validation --
+  static const String tEmailCannotEmpty = "Email cannot be empty";
+  static const String tInvalidEmailFormat = "Invalid email format";
+  static const String tNoRecordFound = "No record found";
+
+  // -- SnackBar --
+  static const String tAlert = "Alert";
+  static const String tOhSnap = "Oh Snap";
+  static const String tEmailSent = "Hurray!!! Email is on its way.";
+  static const String tCongratulations = "Congratulations";
+  static const String tEmailLinkToResetPassword = "Email Link To Reset Password";
+  static const String tAccountCreateVerifyEmail = "Account Create Verify Email";
+
+  // -- Splash Screen Text
+  static const String tAppName = "Starter Template";
+  static const String tAppTagLine = "Learn To Code. \nFree For Everyone";
+
+  // -- On Boarding Text
+  static const String tOnBoardingTitle1 = "Build Awesome Apps";
+  static const String tOnBoardingTitle2 = "Learn from YouTube";
+  static const String tOnBoardingTitle3 = "Get Code & Resources";
+  static const String tOnBoardingSubTitle1 = "Let's start your journey with us on this amazing and easy platform.";
+  static const String tOnBoardingSubTitle2 = "Get Video Tutorials of each topic to learn things easily.";
+  static const String tOnBoardingSubTitle3 = "Save time by just copy pasting complete apps you learned from videos.";
+  static const String tOnBoardingCounter1 = "1/3";
+  static const String tOnBoardingCounter2 = "2/3";
+  static const String tOnBoardingCounter3 = "3/3";
+
+  // -- Welcome Screen Text
+  static const String tWelcomeTitle = "Build Awesome Apps";
+  static const String tWelcomeSubTitle = "Let's put your creativity on the development highway.";
+
+  // -- Login Screen Text
+  static const String tLoginTitle = "Welcome Back,";
+  static const String tLoginSubTitle = "Make it work, make it right, make it fast.";
+  static const String tRememberMe = "Remember Me?";
+  static const String tDontHaveAnAccount = "Don't have an Account";
+  static const String tEnterYour = "Enter your";
+  static const String tResetPassword = "Reset Password";
+  static const String tOR = "OR";
+  static const String tConnectWith = "Connect With";
+  static const String tFacebook = "Facebook";
+  static const String tPhoneNumber = "Phone Number";
+  static const String tGoogle = "Google";
+
+  // -- Sign Up Screen Text
+  static const String tSignUpTitle = "Get On Board!";
+  static const String tSignUpSubTitle = "Create your profile to start your Journey.";
+  static const String tAlreadyHaveAnAccount = "Already have an Account";
+
+  // -- Verify Email Screen Text
+  static const String confirmEmail = "Verify your email address!";
+  static const String confirmEmailSubTitle = "Congratulations! Your Account Awaits: Verify Your Email to Start Shopping and Experience a World of Unrivaled Deals and Personalized Offers.";
+  static const String resendEmail = "Resend Email";
+  static const String yourAccountCreatedTitle = "Your account successfully created!";
+  static const String yourAccountCreatedSubTitle = "Welcome to Your Application: Your Account is Created, Unleash the Joy of Seamless Online Experience!";
+
+
+  // -- Forget Password Text
+  static const String tForgetPasswordTitle = "Make Selection!";
+  static const String tForgetPasswordSubTitle = "Select one of the options given below to reset your password.";
+  static const String tResetViaEMail = "Reset via Mail Verification";
+  static const String tResetViaPhone = "Reset via Phone Verification";
+
+  // -- Forget Password Via Phone - Text
+  static const String tForgetPhoneSubTitle = "Enter your registered Phone No to receive OTP";
+
+  // -- Forget Password Via E-Mail - Text
+  static const String tForgetMailSubTitle = "Enter your registered E-Mail to receive OTP";
+
+  // -- OTP Screen - Text
+  static const String tOtpTitle = "CO\nDE";
+  static const String tOtpSubTitle = "Verification";
+  static const String tOtpMessage = "Enter the verification code sent on your Phone Number ";
+
+  // -- Phone Number Authentication - Text
+  static const String phoneNo = '745-628-5429';
+  static const String selectCountry = 'Select Country';
+  static const String signupScreenTitle = "signupScreenTitle";
+  static const String signupScreenSubTitle = "signupScreenSubTitle";
+  static const String otpVerification = "OTP Verification";
+  static const String signInSubTitle = "We will send a one time SMS message.";
+  static const String selectCountryCode = "Select Country Code";
+  static const String sendingOTP = "Sending OTP...";
+  static const String phoneVerifiedTitle = "Phone Verified";
+  static const String phoneVerifiedMessage = "Your phone number has been verified.";
+  static const String noInternet = "No Internet";
+  static const String checkInternetConnection = "Please check your internet connection and try again.";
+  static const String unableToSendOTP = "Unable to send OTP";
+  static const String otpSendTitle = "OTP Send";
+  static const String otpSendMessage = "OTP Send to your phone number successfully.";
+  static const String otpFooter = "Didn’t receive OTP?";
+  static const String otpSubTitle = "Please enter the six digit OTP code that we’ve send to your phone number";
+  static const String enter6digitOTPCode = "Enter 6 digit OTP Code";
+  static const String inText = "in";
+  static const String resendOTP = "Re-Send OTP";
+  static const String thenLets = "Then let’s ";
+
+  // -- Email Verification
+  static const String tEmailVerificationTitle = "Verify your email address";
+  static const String tEmailVerificationSubTitle =
+      "We have just send email verification link on your email. Please check email and click on that link to verify your Email address. \n\n If not auto redirected after verification, click on the Continue button.";
+  static const String tResendEmailLink = "Resend E-Mail Link";
+  static const String tBackToLogin = "Back to login";
+
+  // -- Dashboard Screen - Text
+  static const String tDashboardTitle = "Hey, Coding with T";
+  static const String tDashboardHeading = "Explore Courses";
+  static const String tDashboardSearch = "Search...";
+  static const String tDashboardBannerTitle2 = "JAVA";
+  static const String tDashboardButton = "View All";
+  static const String tDashboardTopCourses = "Top Courses";
+  static const String tDashboardBannerSubTitle = "10 Lessons";
+  static const String tDashboardBannerTitle1 = "Android for Beginners";
+
+  // -- Profile Screen - Text
+  static const String tProfile = "Profile";
+  static const String tEditProfile = "Edit Profile";
+  static const String tLogoutDialogHeading = "Logout";
+  static const String tProfileHeading = "Coding with T";
+  static const String tProfileSubHeading = "superAdmin@codingwitht.com";
+
+  // -- Menu
+  static const String tMenu5 = tLogout;
+  static const String tMenu1 = "Settings";
+  static const String tMenu4 = "Information";
+  static const String tMenu2 = "Billing Details";
+  static const String tMenu3 = "User Management";
+
+  // -- Update Profile Screen - Text
+  static const String tDelete = "Delete";
+  static const String tJoined = "Joined ";
+  static const String tJoinedAt = " 31 October 2022";
+
+  // -- Validation
+  static const String dateOfBirthError = "You must be at least 18 years old.";
+
+  // -- Dashboard 1
+  static const String popularProducts = "Popular Products";
+  static const String homeAppbarTitle = "Good day for shopping";
+}

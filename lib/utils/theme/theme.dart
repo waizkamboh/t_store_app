3df2ba46a319551e5cbdd525576870a6fe4c84eb
waizkamboh <waizkamboh6@gmail.com>
2025-09-23 11:55:35 +0500

    Elevated button theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:t_store/utils/theme/custom_theme/text_theme.dart';
 
 class TAppTheme{
   TAppTheme._();
 
-  static ThemeData lightTheme = ThemeData();
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+  );
 
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
 }

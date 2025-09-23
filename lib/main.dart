8dfaa6906d2322463635a4d302bab10af087ea56
waizkamboh <waizkamboh6@gmail.com>
2025-09-23 16:16:46 +0500

    add color

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
+import 'app.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
 
